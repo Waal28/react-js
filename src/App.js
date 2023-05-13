dc1d2685d33d3730ce00ea45e2bb51a571f2b115
Waal28 <iwalfaizul@gmail.com>
2023-05-13 15:19:07 +0700

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import "./App.css";
-import TodoList from "./components/TodoList";
+import { useState } from "react";
+import styles from "./App.module.css";
+import CounterProvider from "./components/3.hooks/context/CounterProvider";
+import Counter from "./components/3.hooks/counter";
+import LuasLingkaran from "./components/3.hooks/LuasLingkaran";
+import LuasSegitiga from "./components/3.hooks/LuasSegitiga";
+import FunctCounter from "./components/2.state/funcCounter";
 
 function App() {
+  // const [massage, setMassage] = useState("");
+  // const [counter, setCounter] = useState(0);
+
+  // const handleReachMax = () => {
+  //   setMassage("Udah Max");
+  // };
+  const [page, setPage] = useState("LuasSegitiga");
   return (
-    <>
-      <TodoList />
-    </>
+    <div className={styles.App}>
+      <CounterProvider>
+        <button onClick={() => setPage("LuasSegitiga")}>Luas Segitiga</button>
+        <button onClick={() => setPage("LuasLingkaran")}>Luas Lingkaran</button>
+
+        {page === "LuasSegitiga" ? <LuasSegitiga /> : <LuasLingkaran />}
+        <Counter />
+      </CounterProvider>
+    </div>
   );
 }
 
